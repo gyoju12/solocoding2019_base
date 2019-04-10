ce35340afe2ab35f27633508c9e9f7893d0f8f6c
socar-bongdali <bongdali@socar.kr>
2019-04-10 17:44:52 +0900

    - 기본 UI 구성 시작

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' as prefix0;
 
 void main() => runApp(MyApp());
 
@@ -19,12 +20,109 @@ class MyAppState extends State<MyApp> {
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Flutter Demo'), // app bar title
+          title: Text('계산기'), // app bar title
         ),
-        body: Center(
-          child: Text('Hello, world'), // center text
-        ),
+        body: _containerButtons(),
       ),
     );
   }
+
+  Widget _containerButtons() => Container(
+    child: Row(
+      children: <Widget>[
+        _containerNumbers(),
+        _containerOperators()
+      ],
+    ),
+  );
+
+  Widget _containerNumbers() => Column(
+    children: <Widget>[
+      Row(
+        children: <Widget>[
+          FlatButton(
+            child: Text("7"),
+          ),
+          FlatButton(
+            child: Text("8"),
+          ),
+          FlatButton(
+            child: Text("9"),
+          ),
+        ],
+      ),
+      Row(
+        children: <Widget>[
+          FlatButton(
+            child: Text("4"),
+          ),
+          FlatButton(
+            child: Text("5"),
+          ),
+          FlatButton(
+            child: Text("6"),
+          ),
+        ],
+      ),
+      Row(
+        children: <Widget>[
+          FlatButton(
+            child: Text("3"),
+          ),
+          FlatButton(
+            child: Text("2"),
+          ),
+          FlatButton(
+            child: Text("1"),
+          ),
+        ],
+      ),
+      Row(
+        children: <Widget>[
+          FlatButton(
+            child: Text("7"),
+          ),
+          FlatButton(
+            child: Text("8"),
+          ),
+          FlatButton(
+            child: Text("9"),
+          ),
+        ],
+      ),
+      Row(
+        children: <Widget>[
+          FlatButton(
+            child: Text("0"),
+          ),
+          FlatButton(
+            child: Text("."),
+          ),
+          FlatButton(
+            child: Text("="),
+          ),
+        ],
+      ),
+    ],
+  );
+
+  Widget _containerOperators() => Column(
+    children: <Widget>[
+      FlatButton(
+        child: Text("<="),
+      ),
+      FlatButton(
+        child: Text("/"),
+      ),
+      FlatButton(
+        child: Text("*"),
+      ),
+      FlatButton(
+        child: Text("-"),
+      ),
+      FlatButton(
+        child: Text("+"),
+      ),
+    ],
+  );
 }
