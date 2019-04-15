4d3fb1d9252b7ca2a341ba443bd299ca45fd9a78
socar-bongdali <bongdali@socar.kr>
2019-04-15 18:27:56 +0900

    UI 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart' as prefix0;
 
 void main() => runApp(MyApp());
 
@@ -10,6 +9,8 @@ class MyApp extends StatefulWidget {
 }
 
 class MyAppState extends State<MyApp> {
+  String _displayCalculationFormula = '';
+
   @override
   Widget build(BuildContext context) {
     // set material design app
@@ -22,107 +23,150 @@ class MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('계산기'), // app bar title
         ),
-        body: _containerButtons(),
+        body: _containerBody(),
       ),
     );
   }
 
-  Widget _containerButtons() => Container(
+  Widget _containerBody() => Container(
+        child: Column(
+          children: <Widget>[
+            _containerOutput(),
+            _containerInput()
+          ],
+        ),
+      );
+
+  Widget _containerOutput() => Padding(
+    padding: EdgeInsets.all(8.0),
     child: Row(
       children: <Widget>[
-        _containerNumbers(),
-        _containerOperators()
+        Expanded(
+          child: Text(
+            '$_displayCalculationFormula',
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
+        DropdownMenuItem(child: Icon(Icons.more_vert),)
       ],
     ),
   );
 
+  Widget _containerInput() => Row(
+        children: <Widget>[_containerNumbers(), _containerOperators()],
+      );
+
   Widget _containerNumbers() => Column(
-    children: <Widget>[
-      Row(
         children: <Widget>[
-          FlatButton(
-            child: Text("7"),
+          Row(
+            children: <Widget>[
+              RaisedButton(
+                child: Text('7'),
+                onPressed: () { _inputData('7'); },
+              ),
+              RaisedButton(
+                child: Text('8'),
+                onPressed: () { _inputData('8'); },
+              ),
+              RaisedButton(
+                child: Text('9'),
+                onPressed: () { _inputData('9'); },
+              ),
+            ],
+            mainAxisSize: MainAxisSize.max,
           ),
-          FlatButton(
-            child: Text("8"),
+          Row(
+            children: <Widget>[
+              RaisedButton(
+                child: Text('4'),
+                onPressed: () { _inputData('4'); },
+              ),
+              RaisedButton(
+                child: Text('5'),
+                onPressed: () { _inputData('5'); },
+              ),
+              RaisedButton(
+                child: Text('6'),
+                onPressed: () { _inputData('6'); },
+              ),
+            ],
+            mainAxisSize: MainAxisSize.max,
           ),
-          FlatButton(
-            child: Text("9"),
+          Row(
+            children: <Widget>[
+              RaisedButton(
+                child: Text('3'),
+                onPressed: () { _inputData('3'); },
+              ),
+              RaisedButton(
+                child: Text('2'),
+                onPressed: () { _inputData('2'); },
+              ),
+              RaisedButton(
+                child: Text('1'),
+                onPressed: () { _inputData('1'); },
+              ),
+            ],
+            mainAxisSize: MainAxisSize.max,
+          ),
+          Row(
+            children: <Widget>[
+              RaisedButton(
+                child: Text('0'),
+                onPressed: () { _inputData('0'); },
+              ),
+              RaisedButton(
+                child: Text('.'),
+                onPressed: () { _inputData('.'); },
+              ),
+              RaisedButton(
+                child: Text('='),
+                onPressed: () { _inputData('='); },
+              ),
+            ],
+            mainAxisSize: MainAxisSize.max,
           ),
         ],
-      ),
-      Row(
-        children: <Widget>[
-          FlatButton(
-            child: Text("4"),
-          ),
-          FlatButton(
-            child: Text("5"),
-          ),
-          FlatButton(
-            child: Text("6"),
-          ),
-        ],
-      ),
-      Row(
-        children: <Widget>[
-          FlatButton(
-            child: Text("3"),
-          ),
-          FlatButton(
-            child: Text("2"),
-          ),
-          FlatButton(
-            child: Text("1"),
-          ),
-        ],
-      ),
-      Row(
-        children: <Widget>[
-          FlatButton(
-            child: Text("7"),
-          ),
-          FlatButton(
-            child: Text("8"),
-          ),
-          FlatButton(
-            child: Text("9"),
-          ),
-        ],
-      ),
-      Row(
-        children: <Widget>[
-          FlatButton(
-            child: Text("0"),
-          ),
-          FlatButton(
-            child: Text("."),
-          ),
-          FlatButton(
-            child: Text("="),
-          ),
-        ],
-      ),
-    ],
-  );
+      );
 
   Widget _containerOperators() => Column(
-    children: <Widget>[
-      FlatButton(
-        child: Text("<="),
-      ),
-      FlatButton(
-        child: Text("/"),
-      ),
-      FlatButton(
-        child: Text("*"),
-      ),
-      FlatButton(
-        child: Text("-"),
-      ),
-      FlatButton(
-        child: Text("+"),
-      ),
-    ],
-  );
+        children: <Widget>[
+          RaisedButton(
+            child: Text('<='),
+            onPressed: () { _inputData('Del'); },
+          ),
+          RaisedButton(
+            child: Text('/'),
+            onPressed: () { _inputData('/'); },
+          ),
+          RaisedButton(
+            child: Text('*'),
+            onPressed: () { _inputData('*'); },
+          ),
+          RaisedButton(
+            child: Text('-'),
+            onPressed: () { _inputData('-'); },
+          ),
+          RaisedButton(
+            child: Text('+'),
+            onPressed: () { _inputData('+'); },
+          ),
+        ],
+      );
+
+  _inputData(String data) {
+    setState(() {
+      if (data == '.') {
+        if (_displayCalculationFormula.length <= 0 && _displayCalculationFormula.contains('.')) {
+          return;
+        }
+      } else if (data == '=') {
+
+      } else if (data == 'Del') {
+        _displayCalculationFormula = _displayCalculationFormula.substring(0, _displayCalculationFormula.length-1);
+            return;
+      }
+      _displayCalculationFormula += data;
+    });
+  }
 }
